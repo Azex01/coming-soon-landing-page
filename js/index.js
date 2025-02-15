f2032d00c28b449a4b2f65b4f5f344480c23128e
Azex01 <tobegoodman5@gmail.com>
2025-02-15 18:23:02 +0300

    nice

---
+++ js/index.js
@@ -0,0 +1,47 @@
+var countDownDate = new Date();
+countDownDate = new Date(countDownDate.getFullYear(), countDownDate.getMonth(), countDownDate.getDate() + 13);
+
+var x = setInterval(function() {
+    var now = new Date();
+    var diff = countDownDate - now;
+
+    var days = Math.floor(diff / (24 * 60 * 60 * 1000));
+    var hours = Math.floor((diff % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
+    var minutes = Math.floor((diff % (60 * 60 * 1000)) / (60 * 1000));
+    var seconds = Math.floor((diff % (60 * 1000)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    // Check if the countdown is over
+    if (diff < 0) {
+        clearInterval(x);
+        document.getElementById("days").innerHTML = "0";
+        document.getElementById("hours").innerHTML = "0";
+        document.getElementById("minutes").innerHTML = "0";
+        document.getElementById("seconds").innerHTML = "0";
+    }
+}, 1000);
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+    'use strict';
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation');
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function(form) {
+            form.addEventListener('submit', function(event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+
+                form.classList.add('was-validated');
+            }, false);
+        });
+})();
